bbd817c35de4eba1569a759f8152c40c0495adec
Travis Reyburn <treyburn@alteryx.com>
2019-12-05 16:22:21 -0700

    created functions for solution and created input data file

---
+++ src/day_1/day_1.py
@@ -0,0 +1,54 @@
+# --- Day 1: The Tyranny of the Rocket Equation ---
+#
+# Santa has become stranded at the edge of the Solar System while delivering presents to other planets! To accurately
+# calculate his position in space, safely align his warp drive, and return to Earth in time to save Christmas, he needs
+# you to bring him measurements from fifty stars.
+#
+# Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second
+# puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+#
+# The Elves quickly load you into a spacecraft and prepare to launch.
+#
+# At the first Go / No Go poll, every Elf is Go until the Fuel Counter-Upper. They haven't determined the amount of
+# fuel required yet.
+#
+# Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module,
+# take its mass, divide by three, round down, and subtract 2.
+#
+# For example:
+#
+# For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2. For a mass of 14, dividing by 3
+# and rounding down still yields 4, so the fuel required is also 2. For a mass of 1969, the fuel required is 654. For
+# a mass of 100756, the fuel required is 33583. The Fuel Counter-Upper needs to know the total fuel requirement. To
+# find it, individually calculate the fuel needed for the mass of each module (your puzzle input), then add together
+# all the fuel values.
+#
+# What is the sum of the fuel requirements for all of the modules on your spacecraft?
+# Answer: 3278434
+
+
+def fuel_counter_upper(file: str) -> int:
+    total = 0
+    with open(file) as fh:
+        for value in fh:
+            total += fuel_calc(value)
+    return total
+
+
+def fuel_calc(value: str) -> int:
+    fuel_amt = int(int(value) / 3) - 2
+    return fuel_amt
+
+
+def main(file: str):
+    assert fuel_calc('12') == 2
+    assert fuel_calc('14') == 2
+    assert fuel_calc('1969') == 654
+    assert fuel_calc('100756') == 33583
+
+    fuel_req = fuel_counter_upper(file)
+    print(fuel_req)
+
+
+if __name__ == '__main__':
+    main('fuel.txt')
